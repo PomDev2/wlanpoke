ac9481cc35a008252c6651db03b35647a75969ca
PomDev2 <dev@palomoney.com>
2021-03-17 12:48:09 -0400

    Version 0.1.3 3/17/2021
    Improve process list at end of page

--- awstats.sh
+++ awstats.sh
@@ -6,7 +6,7 @@
 #
 # This program is free software under GPL3 as stated in gpl3.txt, included.
 
-Version="0.1.1 3/16/2021"
+Version="0.1.3 3/17/2021"
 
 echo -e "HTTP/1.1 200 OK\r"
 echo "Content-type: text/html"
@@ -20,7 +20,7 @@ elif [[ -r "$trq" ]] ; then     # serve the file if it exists. To ... with the c
 else
   echo -e "<html><head><title>WLAN Statistics</title></head><body><h2>Wireless LAN Statistics</h2>\r\n"
   echo "<pre>"
-  uptime
+  echo `date` "(" `uptime` ")"
   echo
   iwconfig eth1
   echo "</pre><h4>wlanpoke test settings (s,q,f) and failed pings [0..n] prior to recovery</h4><pre>"
@@ -29,7 +29,8 @@ else
   wpa_cli scan_results
   echo "</pre><h4>ar6002 chip statistics</h4><pre>"
   /lib/atheros/wmiconfig --getTargetStats
-  echo "</pre><h4>related processes</h4><pre>"
-  ps a -F
+  echo "</pre><h4>important processes</h4><pre>"
+  #ps xf -o "pid,ppid,sess,pmem,vsz,size,rss,ni,pri,stat,pcpu,start,time,args" | grep '[^]]$'		# 0.1.3 was ps a -F
+  ps xf -o "pid,ppid,sess,pmem,vsz,size,rss,ni,pri,state,pcpu,start,time,args" | grep '[^]]$'		# 0.1.3 stat can show S<s which starts strikeout!!!
   echo -e "</pre>\r\n</body></html>"
 fi
